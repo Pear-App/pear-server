584770c2e559aa5a4a93293c795c526680925087
Ana Chua <anaprimawaty@gmail.com>
2017-10-18 02:26:40 +0800

    Add match migration and model

---
+++ models/match.js
@@ -0,0 +1,58 @@
+'use strict'
+
+module.exports = function (sequelize, Sequelize) {
+  var Matches = sequelize.define('Matches', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    candidate: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id'
+      },
+      allowNull: false,
+      unique: 'uniqueMatch'
+    },
+    single: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id'
+      },
+      allowNull: false,
+      unique: 'uniqueMatch'
+    },
+    friend: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id'
+      },
+      allowNull: false,
+      unique: 'uniqueMatch'
+    },
+    singleChoice: {
+      type: Sequelize.BOOLEAN
+    },
+    friendChoice: {
+      type: Sequelize.BOOLEAN
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    updatedAt: Sequelize.DATE
+  })
+
+  Matches.associate = function (models) {
+    Matches.belongsTo(models.Users, {'as': 'matchCandidate'})
+    Matches.belongsTo(models.Users, {'as': 'matchSingle'})
+    Matches.belongsTo(models.Users, {'as': 'matchFriend'})
+  }
+
+  return Matches
+}
