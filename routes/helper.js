d7655abaa44760570d9f39a04ceed74d8c7f952a
Ana Chua <anaprimawaty@gmail.com>
2017-10-17 15:23:58 +0800

    Update helper and complete get and edit endpoints for user

--- routes/helper.js
+++ routes/helper.js
@@ -1,11 +1,17 @@
 module.exports = {
-  ERROR_MSG: "An error occurred with processing your request",
-  successLog: function (source, msg) {
-    var date = new Date().toLocaleString('en-US', {timeZone: 'Asia/Singapore'})
-    console.log('\n[' + date + '][' + source + '][SUCCESS] ' + msg + '\n')
+  SERVER_ERROR_MSG: 'An error occurred with processing your request',
+  CustomError: function (errorName, serverMsg, clientMsg) {
+    var e = new Error(serverMsg)
+    e.name = errorName
+    e.clientMsg = clientMsg
+    return e
   },
   errorLog: function (source, msg) {
     var date = new Date().toLocaleString('en-US', {timeZone: 'Asia/Singapore'})
     console.log('\n[' + date + '][' + source + '][ERROR] ' + msg + '\n')
+  },
+  successLog: function (source, msg) {
+    var date = new Date().toLocaleString('en-US', {timeZone: 'Asia/Singapore'})
+    console.log('\n[' + date + '][' + source + '][SUCCESS] ' + msg + '\n')
   }
 }
