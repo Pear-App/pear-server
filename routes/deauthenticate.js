b1a2849faa1c34665cd35adc5e6097d463449d13
Ana Chua <anaprimawaty@gmail.com>
2017-10-22 00:18:55 +0800

    Endpoint to deauth user

--- routes/deauthenticate.js
+++ routes/deauthenticate.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const base64url = require('base64url')
 const crypto = require('crypto')
+const models = require('../models')
 const helper = require('./helper')
 const CustomError = helper.CustomError
 
@@ -10,11 +11,11 @@ function parse (signedRequest) {
   var sig = encodedData[0]
   var data = JSON.parse(base64url.decode(encodedData[1]))
 
-  if (!data.algorithm || data.algorithm.toUpperCase() != 'HMAC-SHA256') {
+  if (!data.algorithm || data.algorithm.toUpperCase() !== 'HMAC-SHA256') {
     return Promise.reject(new CustomError('UnknownAlgorithmError', 'Expected HMAC-SHA256', ''))
   }
 
-  var expectedSig = crypto.createHmac('sha256', process.env.PEAR_FB_CLIENT_SECRET).update(encodedData[1]).digest('base64').replace(/\+/g,'-').replace(/\//g,'_').replace('=','')
+  var expectedSig = crypto.createHmac('sha256', process.env.PEAR_FB_CLIENT_SECRET).update(encodedData[1]).digest('base64').replace(/\+/g, '-').replace(/\//g, '_').replace('=', '')
   if (sig !== expectedSig) {
     return Promise.reject(new CustomError('BadJSONSigError', 'Bad JSON signature', ''))
   }
@@ -24,12 +25,31 @@ function parse (signedRequest) {
 
 router.post('/', function (req, res) {
   parse(req.body.signed_request).then(userId => {
-    // TODO
-    console.log(userId)
-    helper.successLog(req.originalUrl, '')
-    return res.send({})
-  }).catch((e) => {
-    if (e.name === 'UnknownAlgorithmError' || e.name === 'BadJSONSigError') {
+    return new Promise(function (resolve, reject) {
+      models.Users.findById(userId).then(user => {
+        if (user) {
+          resolve(user)
+        } else {
+          reject(new CustomError('InvalidUserIdError', `Invalid User id ${userId}`, 'Invalid User id'))
+        }
+      }).catch(e => {
+        reject(e)
+      })
+    })
+  }).then(user => {
+    return user.updateAttributes({
+      isSingle: false
+    })
+  }).then(user => {
+    var _ = models.Friendships.destroy({
+      where: { single: user.id }
+    })
+    Promise.all([_, user]).then(([_, user]) => {
+      helper.successLog(req.originalUrl, `Deauthenticated user ${user.id}`)
+      return res.send({})
+    })
+  }).catch(e => {
+    if (e.name === 'InvalidUserIdError' || e.name === 'UnknownAlgorithmError' || e.name === 'BadJSONSigError') {
       helper.errorLog(req.originalUrl, e)
       return res.status(400).send({})
     } else {
