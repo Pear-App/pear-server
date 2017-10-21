676dcbd0ab2dbe2e47121a89ca3e7f63a2f8defb
Ana Chua <anaprimawaty@gmail.com>
2017-10-21 16:44:03 +0800

    Shuffle candidates for friend swipe list

--- routes/match.js
+++ routes/match.js
@@ -5,6 +5,7 @@ var models = require('../models')
 var helper = require('./helper')
 var CustomError = helper.CustomError
 var SERVER_ERROR_MSG = helper.SERVER_ERROR_MSG
+var Sequelize = require('sequelize')
 
 router.use('*', passport.authenticate(['jwt'], { session: false }), function (req, res, next) {
   next()
@@ -61,7 +62,6 @@ function getSeenCandidates (singleId, friendId) {
 
 router.get('/friend/:id', function (req, res) {
   // TODO: add match algorithm
-  // TODO: shuffle candidates
 
   var friendId = req.user.userId
   var singleId = req.params.id
@@ -87,6 +87,7 @@ router.get('/friend/:id', function (req, res) {
       attributes: {
         exclude: ['facebookToken', 'minAge', 'maxAge', 'sexualOrientation', 'createdAt', 'updatedAt']
       },
+      order: [[Sequelize.fn('RAND')]],
       limit: 10
     })
   }).then(candidates => {
